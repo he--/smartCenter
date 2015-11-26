d59b1f9cf176014ca382a9f23088410a0bbc220b
he-- <heliosouza@gmail.com>
2015-11-26 19:58:30 -0300

    tratamento de excecoes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,24 +5,29 @@ namespace AppBundle\Controller;
 use AppBundle\Mercado\Mercado;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/index", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        $mercado = new Mercado();
-        $mercado->initEstoque();
-        $mercado->persistEstoque();
+        $mercado = $request->getSession()->get('mercado');
         $listaItens = $mercado->getFilaItem();
 
-        $promocao = null;
-        if ($request->isMethod('POST')) {
-           $promocao = $mercado->promocao();
+        try{
+            $promocao = null;
+            if ($request->isMethod('POST')) {
+                $promocao = $mercado->promocao();
+            }
+
+        } catch (\Exception $e) {
+            $request->getSession()->getFlashBag()->add('notice', 'Não ha mais promoçõs!!!');
         }
+
 
         return $this->render('default/index.html.twig', array(
             "promocao"  => $promocao,
@@ -30,4 +35,22 @@ class DefaultController extends Controller
             'base_dir'  => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
+
+
+    /**
+     * @Route("/", name="start")
+     */
+    public function startAction(Request $request)
+    {
+
+        $mercado = new Mercado();
+        $mercado->initEstoque();
+        $mercado->persistEstoque();
+
+        $request->getSession()->set('mercado', $mercado);
+        return $this->redirect('/index');
+
+
+    }
+
 }
